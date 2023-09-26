c891b91368abcbcf4d46944823896f2aa3cd1009
Shanto Noor <shantonoor900@gmail.com>
2023-09-26 17:07:33 +0600

    Navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,51 @@
+import logo from "/logo.svg";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const links = [
+    {
+      text: "Home",
+      to: "/",
+    },
+    {
+      text: "Donation",
+      to: "donation/",
+    },
+    {
+      text: "Statistics",
+      to: "statistics/",
+    },
+  ];
+  return (
+    <div className="flex flex-col md:flex-row gap-6 justify-between items-center">
+      <div className="flex items-center gap-5">
+        <img className="w-[72px] h-[72px]" src={logo} alt="logo" />
+        <div>
+          <h1 className="text-red1 font-bold text-4xl">Donation</h1>
+          <h2 className="text-black117 font-medium tracking-[9.54px] text-lg">Campaign</h2>
+        </div>
+      </div>
+      <div className="flex gap-12">
+        {links.map((link, idx) => {
+          return (
+            <NavLink
+              key={idx}
+              to={link.to}
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "active text-red1 underline font-bold"
+                  : ""
+              }
+            >
+              {link.text}
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
+        red1: "#FF444A",
+        black117: "rgba(11, 11, 11, 0.7)",
       },
-      backgroundColor: {
-      },
+      backgroundColor: {},
     },
   },
   plugins: [],
