8378a6747e13be5e9a16782e7ce3e24ab2b7a088
Shanto Noor <shantonoor900@gmail.com>
2023-09-26 16:06:08 +0600

    complete fake data creation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const data = useLoaderData();
+  return (
+    <>
+      <div>Home</div>
+      <div className="grid grid-cols-4">
+        {data.map((item) => (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <img src={item.picture} alt="" />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
