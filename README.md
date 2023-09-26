a14e7b280d8914d3e8aa3d4a05b0b48419166384
Shanto Noor <shantonoor900@gmail.com>
2023-09-26 12:12:49 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Course-Registration
+
+## Live Link
+
+Netlify: [https://course-registration900.netlify.app/](https://course-registration900.netlify.app/)
+
+Surge: [http://course-registration900.surge.sh/](http://course-registration900.surge.sh/)
+
+##  Questions
+
+- Add at least 3 Project features 
+  
+- Discuss how you managed the state in your assignment project.
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import Donation from "./pages/Donation";
+import Statistics from "./pages/Statistics";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "donation/",
+        element: <Donation></Donation>,
+      },
+      {
+        path: "statistics/",
+        element: <Statistics></Statistics>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
