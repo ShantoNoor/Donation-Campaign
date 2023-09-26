bac34688bb04456c72087c9baeea925d8a9b1e7b
Shanto Noor <shantonoor900@gmail.com>
2023-09-26 22:37:00 +0600

    Search bar complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,22 @@ const Home = () => {
   const data = useLoaderData();
   return (
     <>
-      <div >Home</div>
+      <h3 className="mt-28 text-5xl text-black111 text-center font-bold">
+        I Grow By Helping People In Need
+      </h3>
+
+      <div className="flex mt-10 justify-center">
+        <input
+          className="border border-[#DEDEDE] h-12 w-[470px] p-4 rounded-tl-lg rounded-bl-lg"
+          type="search"
+          name="search"
+          placeholder="Search here...."
+        />
+        <button className="bg-red1 text-white px-7 rounded-tr-lg rounded-br-lg">
+          Search
+        </button>
+      </div>
+
       <div className="grid grid-cols-4">
         {data?.map((item) => (
           <div key={item.id}>
